df0c0072411f03ed8c5788375f9bb9ed138d3a49
tao.haiwu <tao.haiwu@ofilm.com>
2019-08-27 19:23:21 +0800

    增加share memory

--- ipc_share_mem_test/share_mem0/main.c
+++ ipc_share_mem_test/share_mem0/main.c
@@ -2,8 +2,51 @@
 #include "mysem.h"
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/shm.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/shm.h>
+
 
 #if 1
+
+
+#define SHARE_MEM_SIZE 0Xff
+
+
+
+int sharememory(int ipc_size,int flag)
+{
+	int id;
+	key_t key=ftok("/tmp",66);
+	if(key < 0)
+	{
+		printf("get key error\n");
+		return -1;
+	}
+	id = shmget(key,ipc_size,flag);
+	if(id < 0)
+	{
+		printf("get id error\n");
+		return -1;
+	}
+	return id;
+}
+ 
+int create_ipc(int ipc_size)
+{
+	return sharememory(ipc_size,IPC_CREAT|IPC_EXCL|0666);
+}
+int get_ipc(int ipc_size)
+{
+	return sharememory(ipc_size,IPC_CREAT|0666);
+}
+int destroy_sharememory(int id)
+{
+	return shmctl(id,IPC_RMID,NULL);
+}
+
 
 int quit_flag = 0;
 
@@ -20,18 +63,39 @@ void handler(int sig)
 int main()
 {
     int semid = create_sems(10); // 创建一个包含10个信号量的信号集
-
+    int ret=0;
+    int share_mem_id;
+    
     printf("i am share memory 0\n");
     
     init_sems(semid, 0, 1);  // 初始化编号为 0 的信号量值为1
     //init_sems(semid, 1, 1);  // 初始化编号为 1 的信号量值为1
-
+    
+    share_mem_id=create_ipc(SHARE_MEM_SIZE);
+    if(share_mem_id==-1)
+    {
+        printf("create ipc error");
+        return -1;    
+    }
+    unsigned char *share_mem_addr=shmat(share_mem_id,NULL,0);
+    if(share_mem_addr<0)
+    {
+        printf("shmat eroor \n ");
+        return -1;
+    }
+    
     signal(SIGINT,handler);
     
     while(quit_flag!=1)
     {
         P(semid,0);
-        printf("1 在吗？\n");
+        printf("write data to share mem\n");
+        memset(share_mem_addr,0,SHARE_MEM_SIZE);
+        for(int i=0;i<SHARE_MEM_SIZE;i++)
+        {
+            share_mem_addr[i]=i;
+        }
+        
         fflush(stdout);
         sleep(3);
         V(semid, 0);
@@ -44,7 +108,7 @@ int main()
         
     }
         
-
+    shmctl(share_mem_id,IPC_RMID,NULL);
     destroy_sems(semid);
     return 0;
 }
--- ipc_share_mem_test/share_mem1/main.c
+++ ipc_share_mem_test/share_mem1/main.c
@@ -2,6 +2,43 @@
 #include "mysem.h"
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/shm.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/shm.h>
+
+#define SHARE_MEM_SIZE 0Xff
+
+
+
+int sharememory(int ipc_size,int flag)
+{
+	int id;
+	key_t key=ftok("/tmp",66);
+	if(key < 0)
+	{
+		printf("get key error\n");
+		return -1;
+	}
+	id = shmget(key,ipc_size,flag);
+	if(id < 0)
+	{
+		printf("get id error\n");
+		return -1;
+	}
+	return id;
+}
+ 
+int create_ipc(int ipc_size)
+{
+	return sharememory(ipc_size,IPC_CREAT|IPC_EXCL|0666);
+}
+int get_ipc(int ipc_size)
+{
+	return sharememory(ipc_size,IPC_CREAT|0666);
+}
+
 
 #if 1
 int main()
@@ -10,11 +47,33 @@ int main()
     if(sem_id<0)
         return -1 ;
 
+    int ret=0;
+    int share_mem_id;
+    
+
+    share_mem_id =get_ipc(SHARE_MEM_SIZE);
+    if(ret==-1)
+        return -1;
+
+    unsigned char *share_mem_addr=shmat(share_mem_id,NULL,0);
+    if(share_mem_addr<0)
+    {
+        printf("shmat eroor \n ");
+        return -1;
+    }
+
     printf("i am share memory 1 \n");
     while(1)
     {
         P(sem_id, 0); // 对该信号量集中的0号信号  做P操作
-        printf("你好 i am 1\n");
+        printf("read share mem\n");
+        for(int i=0;i<SHARE_MEM_SIZE;i++)
+        {
+            if(share_mem_addr[i]!=i)
+            {
+                printf("share read error index:%d",i);
+            }
+        }
         fflush(stdout);
         V(sem_id, 0);
     }
